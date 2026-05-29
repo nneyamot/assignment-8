134e321f6595c1f7ac42c961728cdbd238ff38fa
nneyamot <mohammadnneyamot@gmail.com>
2026-05-29 18:15:22 +0600

    3rd commit

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: process.env.BETTER_AUTH_URL
+    baseURL: "https://assignment-8-gray.vercel.app"
 })
 export const { signIn, signUp, useSession } = createAuthClient()
