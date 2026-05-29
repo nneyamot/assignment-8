b2c4a33c4350b0fc1ce9e9008df0242c2d351524
nneyamot <mohammadnneyamot@gmail.com>
2026-05-29 18:26:10 +0600

    4th commit

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react"
 export const authClient = createAuthClient({
-    baseURL: "https://assignment-8-gray.vercel.app"
+    baseURL:process.env.BETTER_AUTH_URL
 })
 export const { signIn, signUp, useSession } = createAuthClient()
--- src/lib/dataFetch.js
+++ src/lib/dataFetch.js
@@ -1,14 +1,15 @@
 
 export const getAnimals = async () => {
-    const res = await fetch('https://assignment-8-gray.vercel.app/animals');
+    const res = await fetch('https://assignment-8-jt3irkge9-gaffer-s-projects.vercel.app/animals');
     const animals = await res.json();
     //console.log(animals.length);
     return animals;
 }
 
-export const getIdDetails = async(id) => {
-const res = await fetch(`https://assignment-8-gray.vercel.app/animals/${id}`);
+export const getIdDetails = async (id) => {
+    const res = await fetch(`https://assignment-8-jt3irkge9-gaffer-s-projects.vercel.app/animals/${id}`);
     const animal = await res.json();
     //console.log(animals.length);
     return animal;
 }
+// https://assignment-8-gray.vercel.app
