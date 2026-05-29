e21e3c2599b77bffaf88d9fc5ae40ad21fe8d89d
nneyamot <mohammadnneyamot@gmail.com>
2026-05-29 18:38:00 +0600

    6th commit

--- src/lib/dataFetch.js
+++ src/lib/dataFetch.js
@@ -1,13 +1,13 @@
 
 export const getAnimals = async () => {
-    const res = await fetch('https://assignment-8-gray.vercel.app/animals');
+    const res = await fetch('https://animals-server.onrender.com/animals');
     const animals = await res.json();
     //console.log(animals.length);
     return animals;
 }
 
 export const getIdDetails = async(id) => {
-const res = await fetch(`https://assignment-8-gray.vercel.app/animals/${id}`);
+const res = await fetch(`https://animals-server.onrender.com/animals/${id}`);
     const animal = await res.json();
     //console.log(animals.length);
     return animal;
